a150ec41731fd8973a87d8285a05eecca650e3c4
Michael Rubino <parkcathedralgrace@gmail.com>
2019-04-19 15:30:46 -0400

    Objects move

--- src/app/drag/move.directive.ts
+++ src/app/drag/move.directive.ts
@@ -1,19 +1,43 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 import { DragDirective } from './drag.directive';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+
+interface Position{
+  x:number;
+  y:number;
+}
 
 @Directive({
   selector: '[appMove]'
 })
 export class MoveDirective extends DragDirective{
-  @HostListener('dragStart') onDragStart(){
-    console.log("BOO")
+  @HostBinding('style.transform') get transform():SafeStyle{
+    return this.sanitizer.bypassSecurityTrustStyle(`translateX(${this.position.x}px) translateY(${this.position.y}px)`);
+  }
+  
+  private position:Position = {
+    x:0,
+    y:0
+  }
+  private startPos:Position;
+
+  constructor(private sanitizer: DomSanitizer){
+    super();
   }
 
-  @HostListener('dragMove') onDragMove(){
-    console.log("BOOP")
+  @HostListener('dragStart', ['$event']) onDragStart(event:PointerEvent){
+    this.startPos = {
+      x:event.clientX - this.position.x,
+      y:event.clientY - this.position.y
+    }
   }
 
-  @HostListener('dragEnd') onDragEnd(){
-    console.log("BOOBOO")
+  @HostListener('dragMove', ['$event']) onDragMove(event:PointerEvent){
+    this.position.x = event.clientX - this.startPos.x;
+    this.position.y = event.clientY - this.startPos.y;
+  }
+
+  @HostListener('dragEnd', ['$event']) onDragEnd(event:PointerEvent){
+    
   }
 }
